8b9aae43d78640af0213babb77164014a26f8582
dannyliu <h152074716@gmail.com>
2018-12-01 19:37:40 +0800

    現有程式碼優化

---
+++ ToDoList/Models/obj/TokenCheckObj.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models.obj
+{
+    public class TokenCheckObj
+    {
+        public bool Status { get; set; }
+
+        public string Account { get; set; }
+
+        public string IP { get; set; }
+
+        public string ErrMsg { get; set; }
+    }
+}
